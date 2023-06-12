1b680c6d9fa4f275651e4392ff31ac38dbffc108
Levi Deang <arron.levi1@gmail.com>
2023-06-12 16:20:57 +0800

    Removed codes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 
 const comfartaa = Comfortaa({
   subsets: ["latin"],
-  variable: "--font-lato",
   display: "swap",
   weight: "400",
 });
@@ -15,7 +14,7 @@ export const metadata: Metadata = {
   description: "Levi Deang's resume.",
   keywords: ["React", "NextJS", "NodeJS", "Software developer"],
 };
-//
+
 export default function RootLayout({
   children,
 }: {
@@ -24,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${comfartaa.className} p-2  md:py-6 min-h-screen bg-gradient-to-br from-sky-500 via-sky-600 to-blue-600`}
+        className={`${comfartaa.className} p-2 md:py-6 min-h-screen bg-gradient-to-br from-sky-500 via-sky-600 to-blue-600`}
       >
         {children}
         <Analytics />
