675dbc9b2789fb17c085addcf735bbb9347ce1dd
Levi Deang <arron.levi1@gmail.com>
2023-06-12 15:00:23 +0800

    Added components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,178 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main
+      className="max-w-5xl m-auto bg-white/90 backdrop-blur-md rounded shadow-md p-6"
+      id="main"
+    >
+      <header className="text-sm flex gap-6 flex-col md:flex-row">
+        <div className="flex-[2_1_0%]">
+          <h1 className="text-4xl">Levi Deang</h1>
+          <p className="pt-4 max-w-lg">
+            Passionate front-end software engineer committed to delivering
+            promised results and remaining true to a unique artistic vision.
+          </p>
         </div>
-      </div>
+        <nav className="flex-1 flex flex-col justify-center">
+          <Link href="mailto:arron.levi1@gmail.com">arron.levi1@gmail.com</Link>
+          <Link href="tel:+639230225111">+63 923 022 5111</Link>
+          <Link target="_blank" href="https://github.com/levi-ren">
+            github.com/levi-ren
+          </Link>
+          <Link target="_blank" href="https://www.levideang.dev/">
+            levideang.dev
+          </Link>
+          <Link target="_blank" href="https://www.linkedin.com/in/levi-deang/">
+            linkedin.com/in/levi-deang
+          </Link>
+        </nav>
+      </header>
+      <section className="pt-6 text-zinc-500 flex gap-10 flex-col md:flex-row">
+        <article className="flex-[2_1_0%]">
+          <b className="text-sky-600">EXPERIENCE</b>
+          <ol className="space-y-4 mb-4">
+            <li className="space-y-1">
+              <b className="inline-block text-black">MDI Novare</b>
+              {" — "}
+              <em className="inline-block text-black">
+                Senior Software Developer
+              </em>
+              <time className="text-sm block">January 2022 - PRESENT</time>
+              <p>Lead front-end developer</p>
+              <ul className="list-disc pl-8 text-justify">
+                <li>
+                  Deliver high-quality production code for a number of clients
+                  including Jollibee Foods Corporation, and Bayad Center.
+                </li>
+                <li>
+                  Worked alongside project owners and provided development and
+                  technical solutions to fulfill business requirements.
+                </li>
+                <li>
+                  Worked closely with other developers and other team members to
+                  locate and fix issues.
+                </li>
+                <li>
+                  Lead front-end development and transformed business
+                  requirements into reality.
+                </li>
+              </ul>
+            </li>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <li className="space-y-1">
+              <b className="inline-block text-black">Yoonet</b>
+              {" — "}
+              <em className="inline-block text-black">Software Developer</em>
+              <time className="text-sm block">April 2020 - July 2021</time>
+              <p>Team lead and Angular (2) developer.</p>
+              <ul className="list-disc pl-8 text-justify">
+                <li>
+                  Front-end lead and developed web-application for an Australian
+                  client.
+                </li>
+                <li>
+                  Ensured team members abide by the rules and regulations set by
+                  the company and the client.
+                </li>
+                <li>
+                  Translated client&apos;s existing desktop app to a web-app
+                  using Angular.
+                </li>
+                <li>
+                  Created, maintained, and integrated new features to the
+                  web-app.
+                </li>
+              </ul>
+            </li>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <li className="space-y-1">
+              <b className="inline-block text-black">Quantum Crowd</b>
+              {" — "}
+              <em className="inline-block text-black">
+                Associate Software Developer
+              </em>
+              <time className="text-sm block">August 2019 - March 2020</time>
+              <p>Web 3 and Front-end developer trainee.</p>
+              <ul className="list-disc pl-8 text-justify">
+                <li>
+                  Learned basics of front-end development ie. HTML, CSS,
+                  JavaScript.
+                </li>
+                <li>
+                  Learned basics of back-end development ie. API, status codes,
+                  HTTP verbs, etc.
+                </li>
+                <li>Learned front-end to back-end integrations.</li>
+              </ul>
+            </li>
+          </ol>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <b className="text-sky-600">PROJECTS</b>
+          <ul className="space-y-4 mt-2">
+            <li className="space-y-2">
+              <div>
+                <b className="inline-block text-black">Portfolio</b>
+                <p className="text-sm">Showcase</p>
+              </div>
+              <p className="">
+                Comprehensive representation of my professional and creative
+                work. Created with Next JS and Tailwind.
+              </p>
+            </li>
+            <li className="space-y-2">
+              <div>
+                <b className="inline-block text-black">Parcel</b>
+                <p className="text-sm">E-commerce</p>
+              </div>
+              <p className="">
+                A rich and responsive e-commerce platform with a variety of
+                products to choose from. Fullstack application built from
+                NextJS, MongoDB, and Tailwind.
+              </p>
+            </li>
+            <li className="space-y-2">
+              <div>
+                <b className="inline-block text-black">Tempo</b>
+                <p className="text-sm">Weather app</p>
+              </div>
+              <p className="">
+                Sleek weather app, offering real-time updates, immersive
+                visuals, and daily personalized forecasts. Built with ReactJS
+                and Tailwind.
+              </p>
+            </li>
+          </ul>
+        </article>
+        <article className="flex-1">
+          <b className="text-sky-600">SKILLS</b>
+          <ul className="space-y-4">
+            <li>
+              <b className="inline-block text-black">Front-end Development:</b>
+              <p>Javascript, Typescript, HTML, CSS</p>
+            </li>
+            <li>
+              <b className="inline-block text-black">Back-end Development:</b>
+              <p>NodeJS, MongoDB, Mongoose</p>
+            </li>
+            <li>
+              <b className="inline-block text-black">Frameworks</b>
+              <p>
+                NextJs, ReactJS, Angular 2, Tailwind, Material UI, Express JS
+              </p>
+            </li>
+            <li>
+              <b className="inline-block text-black">Tools & Platforms</b>
+              <p>Git, GitHub, Vercel, Jira</p>
+            </li>
+          </ul>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <b className="text-sky-600 block mt-4">EDUCATION</b>
+          <b className="block mt-1">Tomas del Rosario College</b>
+          <p className="font-medium mt-1">BS Computer Science</p>
+          <time className="block mt-1">July 2015 - May 2019</time>
+        </article>
+      </section>
     </main>
-  )
+  );
 }
